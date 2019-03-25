05a866713fba5e258fd18734eb0ebe54f441b63d
cinthya <cinthysophia@gmail.com>
2019-03-25 18:30:09 +0100

    Ejercicios 3

---
+++ src/main/java/com/cinthyasophia/tema11/Ejercicio03/CocheCambioAutomatico.java
@@ -0,0 +1,8 @@
+package com.cinthyasophia.tema11.Ejercicio03;
+
+public class CocheCambioAutomatico extends Coche{
+
+    public CocheCambioAutomatico(String matricula) {
+        super(matricula);
+    }
+}
---
+++ src/main/java/com/cinthyasophia/tema11/Ejercicio03/Ejercicio3.java
@@ -0,0 +1,9 @@
+package com.cinthyasophia.tema11.Ejercicio03;
+
+public class Ejercicio3 {
+
+    public Ejercicio3(){
+        CocheCambioManual c = new CocheCambioManual("w");
+
+    }
+}
