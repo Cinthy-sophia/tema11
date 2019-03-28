1da350e06c950a4e82e5e1b1b006adcc642f3995
cinthya <cinthysophia@gmail.com>
2019-03-28 19:18:10 +0100

    Merge branch 'master' of https://github.com/Cinthy-sophia/tema11
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/cinthyasophia/tema11/Ejercicio04/Ejercicio4.java
+++ src/main/java/com/cinthyasophia/tema11/Ejercicio04/Ejercicio4.java
@@ -1,4 +1,10 @@
 package com.cinthyasophia.tema11.Ejercicio04;
 
 public class Ejercicio4 {
+    public Ejercicio4(){
+        Electrodomestico e = new Electrodomestico("azul",'G',3,12);
+        Electrodomestico p = new Electrodomestico("azul",'A',3,12);
+        System.out.println(e.toString());
+        System.out.println(p.toString());
+    }
 }
--- src/main/java/com/cinthyasophia/tema11/Ejercicio04/Electrodomestico.java
+++ src/main/java/com/cinthyasophia/tema11/Ejercicio04/Electrodomestico.java
@@ -1,10 +1,18 @@
 package com.cinthyasophia.tema11.Ejercicio04;
 
+
+import java.util.HashMap;
+
 public class Electrodomestico {
+    public enum Consumo {A,B,C,D,E,F}
+    public enum Color {BLANCO,ROJO,NEGRO,AZUL,GRIS}
+
     private static final double PRECIO_BASE=100;
-    private static final String COLOR_DEFAULT= Color.BLANCO.getColor();
-    private static final char CONSUMO_DEFAULT= Consumo.F.getLetra();
+    private static final String COLOR_DEFAULT= Color.BLANCO.toString();
+    private static final char CONSUMO_DEFAULT= Consumo.F.toString().charAt(0);
     private static final double PESO_DEFAULT= 5;
+
+
 
     protected String color;
     protected char consumoEnergetico;
@@ -12,22 +20,83 @@ public class Electrodomestico {
     protected double precio;
 
     public Electrodomestico(String color, char consumoEnergetico, double peso, double precio) {
-        this.color = color;
-        this.consumoEnergetico = consumoEnergetico;
+        this.color = comprobarColor(color);
+        this.consumoEnergetico = compobarConsumoEnergetico(consumoEnergetico);
         this.peso = peso;
         this.precio = precio;
     }
 
     public Electrodomestico() {
-        this.color = COLOR_DEFAULT;
-        this.consumoEnergetico = CONSUMO_DEFAULT;
-        this.peso = PESO_DEFAULT;
-        this.precio = PRECIO_BASE;
+        this(COLOR_DEFAULT,CONSUMO_DEFAULT,PESO_DEFAULT,PRECIO_BASE);
     }
 
     public Electrodomestico(double peso, double precio) {
-        this.peso = peso;
-        this.precio = precio;
+        this(COLOR_DEFAULT,CONSUMO_DEFAULT,peso,precio);
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public char getConsumoEnergetico() {
+        return consumoEnergetico;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    private char compobarConsumoEnergetico(char letra){
+        char consum= ' ';
+        for (Consumo consumo: Consumo.values()) {
+           if (consumo.toString().charAt(0)==letra){
+               consum=consumo.toString().charAt(0);
+           }
+
+        }
+        if (Character.isWhitespace(consum)){
+            consum= CONSUMO_DEFAULT;
+        }
+        return consum;
+    }
+    private String comprobarColor(String color){
+        String colorEl= null;
+        for (Color c: Color.values()) {
+           if (c.toString().equalsIgnoreCase(color)){
+               colorEl=c.toString();
+           }
+
+        }
+        if (colorEl==null){
+            colorEl= COLOR_DEFAULT;
+        }
+        return colorEl;
+    }
+    public double precioFinal(){
+
+        switch (consumoEnergetico){
+            case 'A':
+                precio+=100;
+            break;
+
+            case 'B':
+
+        }
+
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Electrodomestico{" +
+                "color='" + color + '\'' +
+                ", consumoEnergetico=" + consumoEnergetico +
+                ", peso=" + peso +
+                ", precio=" + precio +
+                '}';
+    }
 }
