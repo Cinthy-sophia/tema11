274d2abdae6c71294ee0d31e98abdf603eaa296d
cinthya <cinthysophia@gmail.com>
2019-04-18 14:19:33 +0200

    Ejercicio 7. Menus hechos.

---
+++ src/main/java/com/cinthyasophia/tema11/Ejercicio07/Ejercicio7.java
@@ -0,0 +1,37 @@
+package com.cinthyasophia.tema11.Ejercicio07;
+
+import com.cinthyasophia.tema11.Util.Lib;
+
+public class Ejercicio7 {
+    Lib lib = new Lib();
+
+    public int menuPrincipal(){
+        int opcion;
+        System.out.println("****************");
+        System.out.println("*MENU PRINCIPAL*");
+        System.out.println("****************");
+        System.out.println("1.Nuevo partido.");
+        System.out.println("2.Gestion de entradas.");
+        System.out.println("0.Salir.");
+        System.out.println("****************");
+
+        opcion= lib.validarOpcion(0,2);
+        return opcion;
+    }
+    public int menuPartidos(){
+        int opcion;
+        System.out.println("***************");
+        System.out.println("*MENU PARTIDOS*");
+        System.out.println("***************");
+        System.out.println("1.Venta de entradas.");// todo muestra la matriz con los asientos disponibles y ocupados, y pide el numero deseado
+        System.out.println("2.Devolver una entrada.");
+        System.out.println("3.Listado de los asientos ocupados.");
+        System.out.println("4.Listado de las asientos libres.");
+        System.out.println("5.Mostrar recaudacion del partido.");
+        System.out.println("0.Volver al menu principal.");
+        System.out.println("****************");
+
+        opcion= lib.validarOpcion(0,2);
+        return opcion;
+    }
+}
